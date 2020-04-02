d3df18e7a6fa4449952745ab69a077e5b665b8d7
ShivamBhirud <shivambhirudpr@gmail.com>
2020-04-02 19:03:42 +0530

    Initial commit, Login and Logout is up and running

---
+++ producthunt/urls.py
@@ -0,0 +1,11 @@
+from products import views
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
+]
+
